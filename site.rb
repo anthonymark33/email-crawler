fa489874537adde2010947cf70609645d2c09f62
Mandeep Ahlawat <mandeep052@gmail.com>
2017-07-13 17:14:02 +0530

    removed uri from the output msg

--- site.rb
+++ site.rb
@@ -3,5 +3,5 @@ require_relative 'data'
 
 ARGV.each do |site|
   site = Site.create(:host => site, :created_at => Time.now)
-  $stderr.puts "inserted #{uri.host} into sites" if site.saved?
+  $stderr.puts "inserted #{site} into sites" if site.saved?
 end

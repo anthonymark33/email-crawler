b767bc18dcfb460a88b093e43315abaf364ed889
Mandeep Ahlawat <mandeep052@gmail.com>
2017-07-13 17:09:55 +0530

    store url directly

--- site.rb
+++ site.rb
@@ -2,7 +2,6 @@ require 'uri'
 require_relative 'data'
 
 ARGV.each do |site|
-  uri = URI(site)
-  site = Site.create(:host => uri.host, :created_at => Time.now)
+  site = Site.create(:host => site, :created_at => Time.now)
   $stderr.puts "inserted #{uri.host} into sites" if site.saved?
 end
